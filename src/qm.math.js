fc300af442db7b7298b0b0e5b9321aa99341559e
edmundo <edhowler@gmail.com>
2015-02-04 03:12:03 -0200

    ok

--- src/qm.math.js
+++ src/qm.math.js
@@ -1,6 +1,6 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+;(function ($, window, document, undefined) {
 	"use strict";
 
 	// undefined is used here as the undefined global variable in ECMAScript 3 is
@@ -18,21 +18,27 @@
 		propertyName: "value"
 	};
 
-	// The actual plugin constructor
-	function Plugin ( element, options ) {
+	function QmMath (element, options) {
 		this.element = element;
-		// jQuery has an extend method which merges the contents of two or
-		// more objects, storing the result in the first object. The first object
-		// is generally empty as we don't want to alter the default options for
-		// future instances of the plugin
-		this.settings = $.extend( {}, defaults, options );
+		this.jElement = $(element);
+		this.settings = $.extend({}, defaults, options);
 		this._defaults = defaults;
 		this._name = pluginName;
 		this.init();
 	}
 
-	// Avoid Plugin.prototype conflicts
-	$.extend(Plugin.prototype, {
+	// Represents a group of elements (which can be more MathNodes)
+	function MathNode (element, options) {
+		this.element = element;
+		this.jElement = $(element);
+		this.settings = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
+
+	// Avoid QmMath.prototype conflicts
+	$.extend(QmMath.prototype, {
 		init: function () {
 			// Place initialization logic here
 			// You already have access to the DOM element and
@@ -40,21 +46,23 @@
 			// and this.settings
 			// you can add more functions like the one below and
 			// call them like so: this.yourOtherFunction(this.element, this.settings).
-			console.log("xD");
+			this.jElement.bind({
+				click: this.onClick
+			});
 		},
-		yourOtherFunction: function () {
-			// some logic
+		onClick: function () {
+			console.log(this);
 		}
 	});
 
 	// A really lightweight plugin wrapper around the constructor,
 	// preventing against multiple instantiations
-	$.fn[ pluginName ] = function ( options ) {
+	$.fn[pluginName] = function (options) {
 		return this.each(function() {
-			if ( !$.data( this, "plugin_" + pluginName ) ) {
-				$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+			if (!$.data(this, "plugin_" + pluginName)) {
+				$.data(this, "plugin_" + pluginName, new QmMath(this, options));
 			}
 		});
 	};
 
-})( jQuery, window, document );
+})(jQuery, window, document);
